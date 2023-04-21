011a8a5eb7939bb2a9774f5f394b9872ed338581
dhevadayatvitoindraj <dhevadayatvitoindraj@mail.ugm.ac.id>
2023-04-21 13:28:57 +0700

    adding initial files and folders

--- app/src/main/java/com/example/challengechapterempat/ui/MainActivity.kt
+++ app/src/main/java/com/example/challengechapterempat/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.challengechapterempat
+package com.example.challengechapterempat.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.challengechapterempat.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
