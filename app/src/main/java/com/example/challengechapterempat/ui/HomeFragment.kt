6871c4c5dd4fb019d31d84c8fe6f1fbda695b140
dhevadayatvitoindraj <dhevadayatvitoindraj@mail.ugm.ac.id>
2023-04-21 13:37:04 +0700

    initial view binding in UI fragment

--- app/src/main/java/com/example/challengechapterempat/ui/HomeFragment.kt
+++ app/src/main/java/com/example/challengechapterempat/ui/HomeFragment.kt
@@ -6,17 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.challengechapterempat.R
+import com.example.challengechapterempat.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
-
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
 }
--- app/src/main/java/com/example/challengechapterempat/ui/LoginFragment.kt
+++ app/src/main/java/com/example/challengechapterempat/ui/LoginFragment.kt
@@ -6,16 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.challengechapterempat.R
+import com.example.challengechapterempat.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
+    private lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = FragmentLoginBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
 }
