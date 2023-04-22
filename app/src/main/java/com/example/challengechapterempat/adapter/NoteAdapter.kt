2f5d10e67d55b439c7037aa17740293453b145fb
dhevadayatvitoindraj <dhevadayatvitoindraj@mail.ugm.ac.id>
2023-04-23 06:55:41 +0700

    modify note adapter

--- app/src/main/java/com/example/challengechapterempat/adapter/NoteAdapter.kt
+++ app/src/main/java/com/example/challengechapterempat/adapter/NoteAdapter.kt
@@ -2,10 +2,15 @@ package com.example.challengechapterempat.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.example.challengechapterempat.R
 import com.example.challengechapterempat.databases_room.NoteData
 import com.example.challengechapterempat.databases_room.NoteDatabase
 import com.example.challengechapterempat.databinding.ItemNoteBinding
+import com.example.challengechapterempat.ui.HomeFragment
+import com.example.challengechapterempat.ui.HomeFragmentDirections
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import java.util.ArrayList
@@ -24,6 +29,25 @@ class NoteAdapter(var listNote: List<NoteData>): RecyclerView.Adapter<NoteAdapte
         holder.binding.tvJudul.text = listNote[position].title.toString()
         holder.binding.tvDate.text = listNote[position].date
         holder.binding.tvContent.text = listNote[position].content
+        holder.binding.ivDelete.setOnClickListener {
+            DBNote = NoteDatabase.getInstance(it.context)
+
+            GlobalScope.async {
+                DBNote?.noteDao()?.deleteNote(listNote[position])
+            }
+        }
+
+        holder.binding.ivEdit.setOnClickListener { view ->
+            val bundle = bundleOf("dataedit" to listNote[position])
+            view.findNavController().navigate(R.id.action_homeFragment_to_editNoteFragment, bundle)
+        }
+
+        holder.binding.itemClick.setOnClickListener { view ->
+            val bundle = bundleOf("detail" to listNote[position])
+            view.findNavController().navigate(R.id.action_homeFragment_to_detailNoteFragment, bundle)
+        }
+
+
 
     }
 
